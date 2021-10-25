30ba960568703c2dc2b24617761dd0029e2ada41
Tobi Adegbuji <adegbuji_tobi@yahoo.com>
2021-10-25 15:23:46 -0400

    Added Enum type

--- src/main/java/com/learnkafka/libraryeventsproducer/controller/LibraryEventsController.java
+++ src/main/java/com/learnkafka/libraryeventsproducer/controller/LibraryEventsController.java
@@ -3,8 +3,10 @@ package com.learnkafka.libraryeventsproducer.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learnkafka.libraryeventsproducer.domain.LibraryEvent;
+import com.learnkafka.libraryeventsproducer.domain.LibraryEventType;
 import com.learnkafka.libraryeventsproducer.producer.LibraryEventProducer;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,22 +14,43 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 @RestController
 @RequestMapping("/v1")
 @AllArgsConstructor
+@Log4j2
 public class LibraryEventsController {
 
     private final LibraryEventProducer libraryEventProducer;
 
     @PostMapping("/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
 
-        //Invokes Kafka producer
-        libraryEventProducer.sendLibraryEvent(libraryEvent);
+        log.info("Before SendLibraryEvent");
+
+        libraryEvent.setLibraryEventType(LibraryEventType.NEW);
+
+        //Invokes async Kafka producer method
+       // libraryEventProducer.sendLibraryEvent(libraryEvent);
+
+        //Invokes sync kafka producer method
+        //var sendResult = libraryEventProducer.sendLibraryEventSynchronous(libraryEvent);
+
+        //Invokes async kafka producer method using send Method with a ProducerRecord argument
+        libraryEventProducer.sendLibraryEvent2(libraryEvent);
+
+
+
+        //log.info("SendResult is: {}", sendResult.toString());
+        log.info("After SendLibraryEvent");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
 
     }
+
+
+    //TODO: Implement put method
 
 
 }
--- src/main/java/com/learnkafka/libraryeventsproducer/producer/LibraryEventProducer.java
+++ src/main/java/com/learnkafka/libraryeventsproducer/producer/LibraryEventProducer.java
@@ -4,22 +4,32 @@ package com.learnkafka.libraryeventsproducer.producer;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learnkafka.libraryeventsproducer.domain.LibraryEvent;
-import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFutureCallback;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 @Component
 @Log4j2
-@AllArgsConstructor
 public class LibraryEventProducer {
 
     private final KafkaTemplate<Integer,String> kafkaTemplate;
 
     private final ObjectMapper objectMapper;
 
+    private String topic = "library-events";
+
+    public LibraryEventProducer(KafkaTemplate<Integer, String> kafkaTemplate, ObjectMapper objectMapper) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
+    }
 
 
     public void sendLibraryEvent(LibraryEvent libraryEvent) throws JsonProcessingException {
@@ -29,13 +39,17 @@ public class LibraryEventProducer {
         var value = objectMapper
                 //Makes Json String Look Nice
                 .writerWithDefaultPrettyPrinter()
-                .writeValueAsString(libraryEvent.getBook());
+                .writeValueAsString(libraryEvent);
 
-        var listenableFuture = kafkaTemplate.sendDefault(key, value);
-        listenableFuture.addCallback(new ListenableFutureCallback<SendResult<Integer, String>>() {
+        var listenableFuture = kafkaTemplate
+                .sendDefault(key, value); //Send default uses the default topic configured in the yml file.
+
+        //Notice this is async. Controller already gives response before the below methods even print their log statements.
+        //The async code is handled in a different thread
+        listenableFuture.addCallback(new ListenableFutureCallback<>() {
             @Override
             public void onFailure(Throwable ex) {
-                handleFailure(key,value,ex);
+                handleFailure(key, value, ex);
             }
 
             @Override
@@ -45,6 +59,77 @@ public class LibraryEventProducer {
         });
 
     }
+
+    public void sendLibraryEvent2(LibraryEvent libraryEvent) throws JsonProcessingException {
+
+        var key = libraryEvent.getLibraryEventId();
+
+        var value = objectMapper
+                //Makes Json String Look Nice
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(libraryEvent);
+
+       var producerRecord = buildProducerRecord(key, value, topic);
+
+
+        var listenableFuture = kafkaTemplate
+                .send(producerRecord); //Send allows you to choose which topic you want to produce to.
+
+        //Notice this is async. Controller already gives response before the below methods even print their log statements.
+        //The async code is handled in a different thread
+        listenableFuture.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                handleFailure(key, value, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<Integer, String> result) {
+                handleSuccess(key, value, result);
+            }
+        });
+
+    }
+
+    private ProducerRecord<Integer,String> buildProducerRecord(Integer key, String value, String topic) {
+
+        List<Header> recordHeaders = List.of(
+                new RecordHeader("event-source", "library-scanner".getBytes()),
+                new RecordHeader("hello-header", "helloWorld".getBytes())
+        );
+
+        return new ProducerRecord<>(topic, null, key,value,recordHeaders);
+    }
+
+
+    public SendResult<Integer, String> sendLibraryEventSynchronous(LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException {
+
+        var key = libraryEvent.getLibraryEventId();
+
+        var value = objectMapper
+                //Makes Json String Look Nice
+                .writerWithDefaultPrettyPrinter()
+                .writeValueAsString(libraryEvent);
+
+        SendResult<Integer, String> sendResult;
+
+        try {
+
+            sendResult = kafkaTemplate.sendDefault(key, value)
+                    .get(); //Makes call sync instead of async
+
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("InterruptedException | ExecutionException: Error sending the message. Exception is {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            log.error("Exception: Error sending the message. Exception is {}", e.getMessage());
+            throw e;
+        }
+
+        return sendResult;
+
+    }
+
 
     private void handleFailure(Integer key, String value, Throwable ex) {
         log.error("Error sending the message. Exception is {}", ex.getMessage());
